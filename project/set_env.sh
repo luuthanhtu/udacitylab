4c45786dc66af167982de3560c2182c19684dd27
TEST <TEST@example.com>
2022-03-27 02:28:08 +0700

    Init dev

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
+#!/bin/bash
+export POSTGRES_USERNAME=root
 export POSTGRES_PASSWORD=udacitylabdatabase
 export POSTGRES_HOST=udacitylabdatabase.cof7g5qbwlgx.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udacitylabdatabase
 export AWS_BUCKET=arn:aws:s3:::udacitylab-tult13-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=testing
 export URL=http://localhost:8100
